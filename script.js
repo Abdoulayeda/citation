2f37969d8989951bc344bf9b3a25d98c8cb4f06f
Abdoulayeda <abdoulayeda93@gmail.com>
2022-02-25 22:35:24 +0000

    Ajout photo

--- script.js
+++ script.js
@@ -3,55 +3,43 @@ let citations =[
 		"La vie est un mystère qu'il faut vivre, et non un problème à résoudre.",
 		'M. Gandhi',
 	],
-	['Le plus grand risque est de ne prendre aucun risque.', 'Mark Zuckerberg'],
-	['Méritez votre statut de leader chaque jour.', 'Mickael Jordan'],
-	['Soyez le changement que vous voulez voir dans le monde.', 'Gandhi'],
-	[
-		'A chaque fois que vous vous retrouvez du même côté que la majorité, il est temps de prendre du recul, et de réfléchir.',
-		'Mark Twain',
-	],
-	[
-		'Seulement ceux qui prendront le risque d’aller trop loin découvriront jusqu’où on peut aller.',
-		'T.S Elliot',
-	],
-	['Le succès c’est tomber sept fois, se relever huit.', 'Proverbe japonais'],
-	[
-		'Dans vingt ans vous serez plus déçus par les choses que vous n’avez pas faites que par celles que vous avez faites. Alors sortez des sentiers battus. Mettez les voiles. Explorez. Rêvez. Découvrez.',
-		'Mark Twain',
-	],
-	[
-		'Si vous attendez pour agir, tout ce que vous gagnerez, avec le temps, c’est de l’âge.',
-		'Brian Tracy',
-	],
-	[
-		'Quand on concentre son attention sur un seul projet, l’esprit suggère constamment des idées et des améliorations qui lui échapperaient s’il était occupé avec plusieurs projets en même temps.',
-		'P.T. Barnum',
-	],
-	[
-		'Se dédier à faire tout ce que l’on peut pour aider les autres à obtenir ce qu’ils veulent, c’est la clé du succès.',
-		'Brian Sher',
-	],
-	[
-		'Si vous pensez que vous êtes trop petit pour avoir de l’impact, essayez d’aller au lit avec un moustique.',
-		'Anita Roddick',
-	],
-	[
-		'Ne jugez pas chaque jour sur ce que vous récoltez, mais sur les graines que vous semez.',
-		'R. L. Stevenson',
-	],
-    [
-		'Le succès, c’est se promener d’échecs en échecs tout en restant motivé.',
-		'Winston Churchill',
-	],
     [
         ' Où la souris se moque du chat, il y a un trou',
         'Proverbe Africain'
    ],
    ["Le bonheur ne s'acquiert pas, il ne réside pas dans les apparences, chacun d'entre nous le construit à chaque instant de sa vie avec son coeur.",
    'Proverbe Africain'
-  
-]
-]
+   ],
+   ["Ce n'est pas le jour de la battue qu'il faut dresser son chien de chasse.", ' Amadou Hampâté Bâ'],
+   ["Un conte est un miroir où chacun peut découvrir sa propre image...", 'Amadou Hampâté Bâ'],
+   ["Ne regrette rien, il faudra toujours continuer à apprendre et à te perfectionner, et ce n'est pas à l'école que tu pourras le faire. L'école donne des diplômes, mais c'est dans la vie qu'on se forme.", ' Amadou Hampâté Bâ'],
+   ["En Afrique, un vieillard qui meurt, c'est une bibliothèque qui brûle.", ' Amadou Hampâté Bâ'],
+   ["Les hommes peuvent atteindre un but commun sans emprunter les mêmes voies.", ' Amadou Hampâté Bâ'],
+   ["Mon commandant, on ne peut m'annoncer une nouvelle plus grave que celle que le destin m'a assignée au jour de ma naissance en me disant : \"tu es entré dans une existence dont tu ne sortiras pas vivant, quoi que tu fasses\", et nulle force humaine ne pourra jamais me loger plus étroitement sur cette terre que je ne le serai dans ma propre tombe. C'est pourquoi aucune mauvaise nouvelle ne peut réellement m'assombrir. j'ai appris à voir venir la mort avec le même calme que je vois tomber la nuit quand le jour décline. A chaque réveil, je me considère comme un condamné en sursis. Mais je ne suis pas pessimiste pour autant, mon commandant, et je ne serais nullement surpris si, un jour, je redevenais le grand chef que j'ai été. La vie est un drame qu'il faut vivre avec sérénité.", 'Amadou Hampâté Bâ'],
+   ["Quand il arrive dans un endroit, le caméléon prend la couleur du lieu. Ce n'est pas de l'hypocrisie ; c'est d'abord la tolérance, et puis le savoir-vivre. Se heurter les uns les autres n'arrange rien. Jamais on n'a rien construit dans la bagarre. La bagarre détruit. Donc la mutuelle compréhension est un grand devoir. Il faudrait toujours chercher à comprendre notre prochain. Si nous existons, il faut admettre que, lui aussi, il existe. ", ' Amadou Hampâté Bâ'],
+   ["La politique n'a ni yeux, ni oreilles, ni cœur; en politique le vrai et le mensonge portent le même pagne, le juste et l'injuste marchent de pair, le bien et le mal s'achètent ou se vendent au même prix.", 'Ahmadou Kourouma'],
+   ["Quand, au moment de la séparation entre deux individus, personne ne ressent de regret, la séparation est arrivée trop tard. ", 'Ahmadou Kourouma'], 
+   [" Le genou ne porte jamais le chapeau quand la tête est sur le cou. ", 'Ahmadou Kourouma'],
+   ["L’esclave qui n’est pas capable d’assumer sa révolte ne mérite pas que l’on s’apitoie sur son sort. Cet esclave répondra seul de son malheur s’il se fait des illusions sur la condescendance suspecte d’un maître qui prétend l’affranchir. Seule la lutte libère . ", 'Thomas Sankara'],
+   ["Un peuple conscient ne saurait confier la défense de sa patrie à un groupe d’hommes quelles que soient leurs compétences. Les peuples conscients assument eux-mêmes la défense de leur patrie ", 'Thomas Sankara'],
+   ["La maladie ne se guérit point en prononçant le nom du médicament, mais en prenant le médicament.", 'Thomas Sankara'],
+   ["Je suis comme un cycliste qui grimpe une pente raide, qui a à gauche et à droite des précipices. Il est obliger de pédaler, de continuer de pédaler, sinon il tombe. ", 'Thomas Sankara'],
+   ["Un militaire sans formation politique, idéologique est un criminel en puissance. ", 'Thomas Sankara'],
+   ["En tant que dirigeant il vous faut être à l'étage, ce qui vous permet de voir très loin ; mais de temps en temps, il faut descendre au rez-de-chaussée pour voir également dans la rue ce qu’y se passe. ", 'Thomas Sankara'],
+   ["On parle du Plan Marshall qui a refait l’Europe économique. Mais l’on ne parle pas du Plan africain qui a permis à l’Europe de faire face aux hordes hitlériennes lorsque leurs économies étaient menacés, leurs stabilités étaient menacées. Qui a sauvé l’Europe ? C’est l’Afrique. On en parle très peu. On parle si peu que nous ne pouvons, nous, être complices de ce silence ingrat. Si les autres ne peuvent pas chanter nos louanges, nous en avons au moins le devoir de dire que nos pères furent courageux et que nos anciens combattants ont sauvé l’Europe et finalement ont permis au monde de se débarrasser du nazisme. ", 'Thomas Sankara'],
+   ["D’autres avant moi ont dit, d’autres après moi diront à quel point s’est élargi le fossé entre les peuples nantis et ceux qui n’aspirent qu’à manger à leur faim, boire à leur soif, survivre et conserver leur dignité. Mais nul n’imaginera à quel point le grain du pauvre a nourri chez nous la vache du riche. ",'Thomas Sankara'],
+   ["Nous ne pouvons laisser à nos seuls ennemis d’hier et d’aujourd’hui le monopole de la pensée, de l’imagination et de la créativité ! ", 'Thomas Sankara'],
+   ["La Révolution démocratique et populaire a besoin d’un peuple de convaincus et non d’un peuple de vaincus, d’un peuple de convaincus et non d’un peuple de soumis qui subissent leur destin ", 'Thomas Sankara'],
+   ["Vous ne pouvez pas accomplir des changements fondamentaux sans une certaine dose de folie. Dans ce cas précis, cela vient de l’anticonformisme, du courage de tourner le dos aux vieilles formules, du courage d’inventer le futur. Il a fallu les fous d’hier pour que nous soyons capables d’agir avec une extrême clarté aujourd’hui. Je veux être un de ces fous. Nous devons inventer le futur. ", 'Thomas Sankara'],
+   ["Une jeunesse mobilisée est dangereuse, une jeunesse mobilisée est une puissance qui effraye même les bombes atomiques.", 'Thomas Sankara'],
+   ["Le système néocolonial tremble quand le peuple devenu maître de sa destinée veut rendre sa justice !", 'Thomas Sankara'],
+   ["Quand le peuple se met debout , l’impérialisme tremble. ", 'Thomas Sankara'],
+   ["L’esprit de liberté, de dignité, de compter sur ses propres forces, d’indépendance et de lutte anti-impérialiste doit souffler du Nord au Sud, du Sud au Nord et franchir allègrement les frontières. D’autant plus que les peuples africains pâtissent des mêmes misères, nourrissent les mêmes sentiments, rêvent des mêmes lendemains meilleurs. ", 'Thomas Sankara'],
+   ["La vraie émancipation de la femme, c’est celle qui responsabilise la femme. ", 'Thomas Sankara'],
+
+
+
+] 
 
 let content = document.querySelector('.content');
 let auteur = document.querySelector('.auteur');
